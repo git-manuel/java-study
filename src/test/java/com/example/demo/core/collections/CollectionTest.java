2ec7aada676b344989112e817327b9b5cb2533e6
manuel <manuel.almeida@koin.com.br>
2022-05-08 15:13:20 -0300

    Updates

--- src/test/java/com/example/demo/core/collections/CollectionTest.java
+++ src/test/java/com/example/demo/core/collections/CollectionTest.java
@@ -1,14 +1,13 @@
-package com.example.demo;
+package com.example.demo.core.collections;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class DemoApplicationTests {
+
+class CollectionTest {
 
 	@Test
 	void contextLoads() {
-		BigDecimalUtils.roundHalfUp(calculateDTO.getCetMonth())
 	}
 
 }
