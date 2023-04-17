f999ea5e94ef9aa7876783c4a93e8f7c4bcde0ee
manuel <manuelmbda@gmail.com>
2023-04-17 17:32:43 +1000

    update

--- src/test/java/com/example/core/collections/ArrayTest.java
+++ src/test/java/com/example/core/collections/ArrayTest.java
@@ -3,9 +3,13 @@ package com.example.core.collections;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class ArrayTest {
 
     private String entry = "AA BB CC DD EE";
+
 
     @Test
     void initiazie2DArrayByCharAT() {
@@ -27,7 +31,7 @@ public class ArrayTest {
         String[][] rectangle = new String[entryArray.length][entryArray[0].length()];
 
         for (int i = 0; i < rectangle.length; i++) {
-          rectangle[i] = entryArray[i].split("");
+            rectangle[i] = entryArray[i].split("");
         }
 
         for (String[] strings : rectangle) {
@@ -37,6 +41,36 @@ public class ArrayTest {
             }
             System.out.println();
         }
+    }
+
+    @Test
+    void sort2DArrayInverse() {
+        String entry = "ZYWR BCDA BCDA";
+
+        String[] entryArray = entry.split(" ");
+        String[][] rectangle = new String[entryArray.length][entryArray[0].length()];
+
+        for (int i = 0; i < rectangle.length; i++) {
+            for (int j = 0; j < rectangle[i].length; j++) {
+                rectangle[i][j] = String.valueOf(entryArray[i].charAt(j));
+
+            }
+        }
+
+
+        //Comparing, given a complex element (a) sort by property (a[0] )
+        Arrays.sort(rectangle, Comparator.comparing(a -> a[0]));
+
+
+        for (int i = 0; i < rectangle.length; i++) {
+            Arrays.sort(rectangle[i]);
+            for (int j = 0; j < rectangle[i].length; j++) {
+                System.out.print(rectangle[i][j]);
+
+            }
+            System.out.println("");
+        }
+
     }
 
 
