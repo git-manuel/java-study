70d6ac7847654a324fe850d4c5edf183b81def65
manuel <manuelmbda@gmail.com>
2023-04-18 15:25:07 +1000

    update

--- src/test/java/com/example/core/collections/ArrayTest.java
+++ src/test/java/com/example/core/collections/ArrayTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
 
 public class ArrayTest {
 
@@ -12,8 +13,9 @@ public class ArrayTest {
 
 
     @Test
-    void initiazie2DArrayByCharAT() {
+    void initialize2DArrayByCharAT() {
         String[] entryArray = entry.split(" ");
+
         String[][] rectangle = new String[entryArray.length][entryArray[0].length()];
 
         for (int i = 0; i < rectangle.length; i++) {
@@ -59,18 +61,41 @@ public class ArrayTest {
 
 
         //Comparing, given a complex element (a) sort by property (a[0] )
-        Arrays.sort(rectangle, Comparator.comparing(a -> a[0]));
+        Arrays.sort(rectangle, Comparator.comparing((String[] a) -> a[0]).reversed());
 
 
         for (int i = 0; i < rectangle.length; i++) {
             Arrays.sort(rectangle[i]);
             for (int j = 0; j < rectangle[i].length; j++) {
                 System.out.print(rectangle[i][j]);
-
             }
             System.out.println("");
         }
+    }
 
+
+    @Test
+    void sort2DArrayInverse2() {
+        int[][] rectangle = {
+                {5, 10, 15, 20},
+                {2, 7, 12, 17},
+                {4, 9, 14, 19},
+                {3, 8, 13, 18},
+                {1, 6, 11, 16}
+        };
+
+
+        //Comparing, given a complex element (a) sort by property (a[0] )
+        Arrays.sort(rectangle, Comparator.comparing((int[] a) -> a[0]).reversed());
+
+
+        for (int i = 0; i < rectangle.length; i++) {
+            Arrays.sort(rectangle[i]);
+            for (int j = 0; j < rectangle[i].length; j++) {
+                System.out.print(rectangle[i][j]);
+            }
+            System.out.println("");
+        }
     }
 
 
--- src/test/java/com/example/core/collections/MapTest.java
+++ src/test/java/com/example/core/collections/MapTest.java
@@ -3,9 +3,11 @@ package com.example.core.collections;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.HashMap;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
+import javax.persistence.criteria.CriteriaBuilder;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class MapTest {
 
@@ -13,13 +15,99 @@ public class MapTest {
     private HashMap<String, Integer> map = new HashMap<>();
 
     @BeforeEach
-    private void init (){
-        map.put("hello",1);
+    private void init() {
+        map.put("hello", 1);
+        map.put("Brazil", 4);
+        map.put("Chile", 3);
+        map.put("France", 3);
+        map.put("Argentina", 2);
+        map.put("Germany", 5);
     }
 
+
     @Test
-    public void NotExistentKeyShouldReturnNull(){
+    public void NotExistentKeyShouldReturnNull() {
 
         assertNull(map.get("not-existent-key"));
     }
+
+    @Test
+    public void verifyIfKeyExist() {
+
+        assertTrue(map.containsKey("hello"));
+    }
+
+    @Test
+    public void verifyIfValueExist() {
+
+        assertTrue(map.containsKey("hello"));
+    }
+
+    @Test
+    public void cantModifyMapWhileInLoop() {
+
+        Exception exception = assertThrows(ConcurrentModificationException.class, () -> {
+            for (String country : map.keySet()) {
+                if (country.equalsIgnoreCase("Brazil")) {
+                    map.remove(country);
+                }
+            }
+        });
+
+        assertTrue(exception instanceof ConcurrentModificationException);
+
+    }
+
+
+    @Test
+    //Entry is a object that represents both key and value
+    public void IterateUsingEntryForach() {
+
+
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            System.out.println(entry.getKey());
+        }
+
+        map.entrySet().stream().forEach(
+                (entry) -> {
+                    System.out.println(entry.getKey());
+                }
+        );
+    }
+
+    @Test
+    //Entry is a object that represents both key and value
+    public void IterateUsingEntryStreams
+            () {
+
+
+        map.entrySet().stream().forEach(
+                (entry) -> {
+                    System.out.println(entry.getKey());
+                }
+        );
+    }
+
+    @Test
+    public void sortMapByKeyUsingList() {
+
+
+        var keys = new ArrayList<>(map.keySet());
+
+        Collections.sort(keys, Comparator.reverseOrder());
+        keys.forEach(System.out::println);
+    }
+
+
+    @Test
+    public void sortMapByKeyUsingStreamReversed() {
+
+
+        var newMap = map.entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByKey(Comparator.reverseOrder())).collect(Collectors.toMap(
+                        Map.Entry::getKey, Map.Entry::getValue, (old, newValue) -> old, LinkedHashMap::new
+                ));
+    }
+
 }
